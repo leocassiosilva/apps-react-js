c72c6c2b209971168a01152b18943262bd2a1275
leocassiosilva <leocassiosilva1234@gmail.com>
2022-12-07 15:11:56 -0300

    0209

---
+++ 0209/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  function handleClick(event) {
+    alert('Comprou: ' + event.target.innerText);
+  }
+
+  return (
+    <div>
+      <button onClick={handleClick}>Camisa</button>
+      <button onClick={handleClick}>Bermuda</button>
+    </div>
+  );
+};
+
+export default App;
